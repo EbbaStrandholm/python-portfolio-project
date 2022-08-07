192245524950105d1ed3367aa909c23241f79bf0
EbbaStrandholm <ebba@strandholm.com>
2022-08-07 21:32:54 +0000

    Add a listofwords.py file which will contain the list of words the hangman game will randomly choose from

---
+++ listofwords.py
@@ -0,0 +1,36 @@
+"""
+A list of words used for the hangman game in game.py
+"""
+list_of_words = [
+    'horse',
+    'house',
+    'apartment',
+    'chocolate',
+    'table',
+    'monstrera',
+    'balcony',
+    'castle',
+    'painting',
+    'computer',
+    'keyboard',
+    'prideflag',
+    'queer',
+    'couch',
+    'butterfly',
+    'shelf',
+    'carpet',
+    'zebra',
+    'money',
+    'haunted',
+    'kitchen',
+    'staircase',
+    'chair',
+    'cloud',
+    'homework',
+    'schoolwork',
+    'espresso',
+    'array',
+    'python',
+    'hangman',
+    'cornsnake'
+]
